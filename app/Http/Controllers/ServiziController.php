30a2569421e00302f05730116dde216494ea9f65
Mindexperiment <andrewloyd@libero.it>
2019-10-15 11:52:44 +0200

    open routes and add servizi controller

--- app/Http/Controllers/ServiziController.php
+++ app/Http/Controllers/ServiziController.php
@@ -8,4 +8,100 @@ class ServiziController extends Controller
     {
         return view('servizi.index');
     }
+
+    public function chirurgia()
+    {
+        return view('services.index', [ 'page' => 'services.pages.chirurgia-page' ])
+            ->with('title', 'Chirurgia orale');
+    }
+
+    public function conservativa()
+    {
+        return view('services.index', [ 'page' => 'services.pages.conservativa-page' ])
+            ->with('title', 'Odontoiatria conservativa');
+    }
+
+    public function estetica()
+    {
+        return view('services.index', [ 'page' => 'services.pages.estetica-page' ])
+            ->with('title', 'Estetica dentale');
+    }
+
+    public function igiene()
+    {
+        return view('services.index', [ 'page' => 'services.pages.igiene-page' ])
+            ->with('title', 'Igiene orale');
+    }
+
+    public function implantologia()
+    {
+        return view('services.index', [ 'page' => 'services.pages.implantologia-page' ])
+            ->with('title', 'Implantologia');
+    }
+
+    public function laser()
+    {
+        return view('services.index', [ 'page' => 'services.pages.laser-page' ])
+            ->with('title', 'Laser terapia');
+    }
+
+    public function medicina()
+    {
+        return view('services.index', [ 'page' => 'services.pages.medicina-page' ])
+            ->with('title', 'Medicina estetica');
+    }
+
+    public function ortodonzia()
+    {
+        return view('services.index', [ 'page' => 'services.pages.ortodonzia-page' ])
+            ->with('title', 'Ortodonzia');
+    }
+
+    public function osteopatia()
+    {
+        return view('services.index', [ 'page' => 'services.pages.osteopatia-page' ])
+            ->with('title', 'Osteopatia');
+    }
+
+    public function paradontologia()
+    {
+        return view('services.index', [ 'page' => 'services.pages.paradontologia-page' ])
+            ->with('title', 'Paradontologia');
+    }
+
+    public function pedodonzia()
+    {
+        return view('services.index', [ 'page' => 'services.pages.pedodonzia-page' ])
+            ->with('title', 'Pedodonzia');
+    }
+
+    public function prgf()
+    {
+        return view('services.index', [ 'page' => 'services.pages.prgf-page' ])
+            ->with('title', 'PRGF ed emoderivati');
+    }
+
+    public function protesi()
+    {
+        return view('services.index', [ 'page' => 'services.pages.protesi-page' ])
+            ->with('title', 'Protesi');
+    }
+
+    public function radiologia()
+    {
+        return view('services.index', [ 'page' => 'services.pages.radiologia-page' ])
+            ->with('title', 'Radiologia');
+    }
+
+    public function roncopatia()
+    {
+        return view('services.index', [ 'page' => 'services.pages.roncopatia-page' ])
+            ->with('title', 'Roncopatia');
+    }
+
+    public function sedazione()
+    {
+        return view('services.index', [ 'page' => 'services.pages.sedazione-page' ])
+            ->with('title', 'Sedazione');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,19 @@
 */
 
 Route::get('/servizi', 'ServiziController@index')->name('servizi.index');
+Route::get( '/servizi/chirurgia', 'ServiziController@chirurgia' )->name( 'services.chirurgia' );
+Route::get( '/servizi/conservativa', 'ServiziController@conservativa' )->name( 'services.conservativa' );
+Route::get( '/servizi/estetica', 'ServiziController@estetica' )->name( 'services.estetica' );
+Route::get( '/servizi/igiene', 'ServiziController@igiene' )->name( 'services.igiene' );
+Route::get( '/servizi/implantologia', 'ServiziController@implantologia' )->name( 'services.implantologia' );
+Route::get( '/servizi/laser', 'ServiziController@laser' )->name( 'services.laser' );
+Route::get( '/servizi/medicina', 'ServiziController@medicina' )->name( 'services.medicina' );
+Route::get( '/servizi/ortodonzia', 'ServiziController@ortodonzia' )->name( 'services.ortodonzia' );
+Route::get( '/servizi/osteopatia', 'ServiziController@osteopatia' )->name( 'services.osteopatia' );
+Route::get( '/servizi/paradontologia', 'ServiziController@paradontologia' )->name( 'services.paradontologia' );
+Route::get( '/servizi/pedodonzia', 'ServiziController@pedodonzia' )->name( 'services.pedodonzia' );
+Route::get( '/servizi/prgf', 'ServiziController@prgf' )->name( 'services.prgf' );
+Route::get( '/servizi/protesi', 'ServiziController@protesi' )->name( 'services.protesi' );
+Route::get( '/servizi/radiologia', 'ServiziController@radiologia' )->name( 'services.radiologia' );
+Route::get( '/servizi/roncopatia', 'ServiziController@roncopatia' )->name( 'services.roncopatia' );
+Route::get( '/servizi/sedazione', 'ServiziController@sedazione' )->name( 'services.sedazione' );
