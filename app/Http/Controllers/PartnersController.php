ffabfe86c535b247c0118a33821473a3678232e4
Mindexperiment <andrewloyd@libero.it>
2019-10-16 11:02:00 +0200

    open routes and controller for partners

--- app/Http/Controllers/PartnersController.php
+++ app/Http/Controllers/PartnersController.php
@@ -8,4 +8,44 @@ class PartnersController extends Controller
     {
         return view('partners.index');
     }
+
+    public function estetica()
+    {
+        return view('partners.estetica');
+    }
+
+    public function fisioterapia()
+    {
+        return view('partners.fisioterapia');
+    }
+
+    public function logopedia()
+    {
+        return view('partners.logopedia');
+    }
+
+    public function odontoiatria()
+    {
+        return view('partners.odontoiatria');
+    }
+
+    public function pedodonzia()
+    {
+        return view('partners.pedodonzia');
+    }
+
+    public function pilates()
+    {
+        return view('partners.pilates');
+    }
+
+    public function protesi()
+    {
+        return view('partners.protesi');
+    }
+
+    public function udito()
+    {
+        return view('partners.udito');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,11 @@
 */
 
 Route::get('/partners', 'PartnersController@index')->name('partners.index');
+Route::get('/partners/estetica', 'PartnersController@estetica')->name('partners.estetica');
+Route::get('/partners/fisioterapia', 'PartnersController@fisioterapia')->name('partners.fisioterapia');
+Route::get('/partners/logopedia', 'PartnersController@logopedia')->name('partners.logopedia');
+Route::get('/partners/odontoiatria', 'PartnersController@odontoiatria')->name('partners.odontoiatria');
+Route::get('/partners/pedodonzia', 'PartnersController@pedodonzia')->name('partners.pedodonzia');
+Route::get('/partners/pilates', 'PartnersController@pilates')->name('partners.pilates');
+Route::get('/partners/protesi', 'PartnersController@protesi')->name('partners.protesi');
+Route::get('/partners/udito', 'PartnersController@udito')->name('partners.udito');
